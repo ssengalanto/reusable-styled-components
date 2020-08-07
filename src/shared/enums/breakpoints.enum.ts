f7c416a554ce53ae832714cb60901c6a7a35cc38
Ssen Galanto <galanto@mclinica.com>
2020-08-07 14:01:34 +0800

    chore: reset styles, create theme

---
+++ src/shared/enums/breakpoints.enum.ts
@@ -0,0 +1,8 @@
+export enum Breakpoints {
+  XS = '20em', // 320px
+  SM = '23.4375em', // 375px
+  MD = '25.875em', // 414px
+  LG = '48em', // 768px
+  XL = '64em', // 1024px
+  XXL = '71.25em', // 1140px
+}
